4d45cc7aa41ab2f3e6fa36cea3c424e887865829
NaniDaChaman <nani00sp@gmail.com>
2024-10-24 02:36:33 -0500

    kubernetes installation

--- install_k8.sh
+++ install_k8.sh
@@ -1,7 +1,19 @@
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gpg
+sudo apt install -y gpg
 sudo mkdir -p -m 755 /etc/apt/keyrings 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o --yes /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+#!sudo rm /etc/containerd/config.toml
+#!containerd config default > config.toml
+#!sudo cp config.toml /etc/containerd/
+sudo systemctl restart containerd.service
+sudo systemctl restart docker.service
+sudo swapoff -a
