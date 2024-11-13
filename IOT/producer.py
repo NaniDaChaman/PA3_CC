e7945a2f4290226e736b291407b1ec8ccef24148
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-11-13 15:13:31 -0600

    y

--- IOT/producer.py
+++ IOT/producer.py
@@ -1,13 +1,29 @@
+#
+#
+# Author: Aniruddha Gokhale
+# CS4287-5287: Principles of Cloud Computing, Vanderbilt University
+#
+# Created: Sept 6, 2020
+#
+# Purpose:
+#
+#    Demonstrate the use of Kafka Python streaming APIs.
+#    In this example, we use the "top" command and use it as producer of events for
+#    Kafka. The consumer can be another Python program that reads and dumps the
+#    information into a database OR just keeps displaying the incoming events on the
+#    command line consumer (or consumers)
+#
+
 import os   # need this for popen
 import time # for sleep
 from kafka import KafkaProducer  # producer of events
-
+from kafka import KafkaConsumer
 # We can make this more sophisticated/elegant but for now it is just
 # hardcoded to the setup I have on my local VMs
 
 # acquire the producer
 # (you will need to change this to your bootstrap server's IP addr)
-producer = KafkaProducer (bootstrap_servers="10.82.35.234:30000", 
+producer = KafkaProducer (bootstrap_servers="129.114.25.80:9092", 
                                           acks=1)  # wait for leader to write to log
 
 # say we send the contents 100 times after a sleep of 1 sec in between
@@ -35,3 +51,30 @@ for i in range (100):
 
 # we are done
 producer.close ()
+    
+
+consumer = KafkaConsumer (bootstrap_servers="129.114.25.80:30000")
+
+# subscribe to topic
+consumer.subscribe (topics=["utilizations"])
+f=open('consumed.txt','w')
+# we keep reading and printing
+for msg in consumer:
+    # what we get is a record. From this record, we are interested in printing
+    # the contents of the value field. We are sure that we get only the
+    # utilizations topic because that is the only topic we subscribed to.
+    # Otherwise we will need to demultiplex the incoming data according to the
+    # topic coming in.
+    #
+    # convert the value field into string (ASCII)
+    #
+    # Note that I am not showing code to obtain the incoming data as JSON
+    # nor am I showing any code to connect to a backend database sink to
+    # dump the incoming data. You will have to do that for the assignment.
+    message =str(msg.value, 'ascii')
+    f.write(message+"\n")
+
+# we are done. As such, we are not going to get here as the above loop
+# is a forever loop.
+f.close()
+consumer.close()
