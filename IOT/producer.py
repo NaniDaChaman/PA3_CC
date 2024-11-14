67f316aff2145c402ca9532786d88c0c9c2313f0
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-11-13 18:57:28 -0600

    .

--- IOT/producer.py
+++ IOT/producer.py
@@ -23,7 +23,7 @@ from kafka import KafkaConsumer
 
 # acquire the producer
 # (you will need to change this to your bootstrap server's IP addr)
-producer = KafkaProducer (bootstrap_servers="10.82.106.108:9092", 
+producer = KafkaProducer (bootstrap_servers="10.0.6.228:9092", 
                                           acks=1)  # wait for leader to write to log
 print("Producer Connected to kafka")
 # say we send the contents 100 times after a sleep of 1 sec in between
@@ -53,7 +53,7 @@ for i in range (10):
 producer.close ()
     
 
-consumer = KafkaConsumer (bootstrap_servers="10.82.106.108:9092")
+consumer = KafkaConsumer (bootstrap_servers="10.0.6.228:9092")
 print("Consumer Connected to kafka")
 # subscribe to topic
 consumer.subscribe (topics=["utilizations"])
