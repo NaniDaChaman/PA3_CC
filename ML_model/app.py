4fd53730514f5d7a6324bfa62a584b3ae5fce594
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-11-26 19:42:22 -0600

    mlmodel created

--- ML_model/app.py
+++ ML_model/app.py
@@ -1,6 +1,8 @@
-from flask import Flask,jsonify,request,url_for,render_template,redirect
+from flask import Flask,jsonify,request,url_for,render_template,redirect,Response
 import model as md
-
+import numpy as np
+import cv2
+#import jsonpickle
 
 
 app= Flask(__name__)
@@ -23,6 +25,24 @@ def handle_form():
     #print(ufile.filename)
     
     return redirect(url_for("get_pred",filename=ufile))
+
+# @app.route('/api/test', methods=['POST'])
+# def test():
+#     r = request
+#     # convert string of image data to uint8
+#     nparr = np.fromstring(r.data, np.uint8)
+#     # decode image
+#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+#     # do some fancy processing here....
+
+#     # build a response dict to send back to client
+#     response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+#                 }
+#     # encode response using jsonpickle
+#     response_pickled = jsonpickle.encode(response)
+
+#     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
 if __name__=="__main__":
