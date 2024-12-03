a58cce61c1bef59bb21580d003111538bb72c826
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-12-03 16:15:28 -0600

    building and testing our arima model

---
+++ ARIMA/discretize.py
@@ -0,0 +1,50 @@
+import uuid
+import json
+import time
+from kafka import KafkaProducer, KafkaConsumer
+import numpy as np
+from datetime import datetime
+import base64
+import arima
+
+try:
+    consumer = KafkaConsumer(
+        'controller',
+        bootstrap_servers='172.16.2.137:30000',#could we try and init in
+        #dynamically with env vars set by kubernetes
+        auto_offset_reset='latest',
+        enable_auto_commit=True,
+        group_id=f'controller-group',
+        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+    )
+    print("Kafka consumer started, listening for inference results.")
+    
+ 
+except Exception as e : 
+    print(f"Error making the Kafka consumer : {e}")
+
+def discretize_consumer(consumer_horizon):
+    arrivals_list=[]
+
+
+start_time=None
+n=0
+arrival_list=[0]
+forecast_list=[]
+for message in consumer:
+    data = message.value 
+    print("\nData from kafka : {data}\n")
+    if n==0:
+        start_time=data['SentTime']
+    elif data['SentTime']>=start_time+120 :
+        arrival_list.append(n)
+        forecast_list=arima.get_prediction(arrival_list,10)
+        print(f"\nnext 10 forecast is : \n{forecast_list}\n")
+        #action=controller(forecast_list)
+        n=0
+        start_time=data['SentTime']
+    n=n+1
+    
+    
+        
+    #arrivals_list.append(data['SentTime'])
